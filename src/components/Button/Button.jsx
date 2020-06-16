fcb2a9149aa0bb951e67e4b65517bf5ece1f8e33
s450026 <maksymilian.kierski@gmail.com>
2020-06-16 19:27:45 +0200

    Add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  // Basic value of our component
+  background-color: #ffd82b;
+  width: ${({ width }) => width || '22rem'};
+  height: 4.7rem;
+  border: none;
+  border-radius: 5rem;
+  font-family: 'Montserrat', serif;
+  font-weight: 500;
+  font-size: 1.6rem;
+  text-transform: uppercase;
+
+  // With secondary props value of our component
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: #e6e6e6;
+      width: 10.5rem;
+      height: 3rem;
+      font-size: 1rem;
+    `}
+`;
+
+export default Button;
--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Button from 'components/Button/Button';
 
-const Root = () => <h1>Hello world!</h1>;
+const Root = () => (
+  <>
+    <h1>This is my new application</h1>
+    <Button width="50rem">Close/save!</Button>
+    <Button>Close/save!</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
 
 export default Root;
