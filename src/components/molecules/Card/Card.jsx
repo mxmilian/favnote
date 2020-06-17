f2f1a8a1d44d2dc239e6a78b7aca77edfbf7cc75
s450026 <maksymilian.kierski@gmail.com>
2020-06-17 21:58:03 +0200

    Add avatar to twitter type card, and link to article type card

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import Heading from 'components/atoms/Heading/Heading';
 import Button from 'components/atoms/Button/Button';
+import linkIcon from 'assets/link.svg';
 
 const StyledWrapper = styled.div`
   min-height: 38rem;
@@ -18,6 +19,11 @@ const StyledWrapper = styled.div`
 const InnerWrapper = styled.div`
   padding: 1.8rem 3.2rem;
   background-color: ${({ theme, activeColor }) => (activeColor ? theme[activeColor] : 'white')};
+  position: relative;
+
+  :first-of-type {
+    z-index: 1;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -25,7 +31,32 @@ const InnerWrapper = styled.div`
       display: flex;
       flex-direction: column;
       justify-content: space-between;
+      padding: 2.6rem 3.2rem;
     `}
+`;
+
+const StyledAvatar = styled.img`
+  height: 8.6rem;
+  width: 8.6rem;
+  border-radius: 5rem;
+  border: 0.4rem solid ${({ theme }) => theme.twitter};
+  position: absolute;
+  right: 25px;
+  top: 15px;
+  background-image: url(${({ src }) => src});
+`;
+
+const StyledLink = styled.a`
+  height: 5.6rem;
+  width: 5.6rem;
+  border-radius: 5rem;
+  border: 0.4rem solid ${({ theme }) => theme.article};
+  position: absolute;
+  right: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: white url(${() => linkIcon}) no-repeat 50% 50%;
+  background-size: 50%;
 `;
 
 const DateInfo = styled(Paragraph)`
@@ -38,6 +69,10 @@ const Card = ({ cardType }) => (
     <InnerWrapper activeColor={cardType}>
       <Heading>Card</Heading>
       <DateInfo>3 days</DateInfo>
+      {cardType === 'twitter' && (
+        <StyledAvatar src="https://secure.img1-ag.wfcdn.com/im/02238154/compr-r85/8470/84707680/pokemon-pikachu-wall-decal.jpg" />
+      )}
+      {cardType === 'article' && <StyledLink href="https://youtube.com" />}
     </InnerWrapper>
     <InnerWrapper flex>
       <Paragraph>
