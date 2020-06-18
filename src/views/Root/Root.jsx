dce70360b6768fdda086070dc00252ec5cc5acc7
s450026 <maksymilian.kierski@gmail.com>
2020-06-18 13:15:59 +0200

    Add others views, and react router

--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import Button from 'components/atoms/Button/Button';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
+import Notes from 'views/Notes/Notes';
+import Twitters from 'views/Twitters/Twitters';
+import Articles from 'views/Articles/Articles';
 
 const Root = () => (
   <MainTemplate>
-    <>
-      <h1>Hello Max!</h1>
-      <Button>Close/save!</Button>
-      <Button secondary>Close/save!</Button>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Notes} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/twitters" component={Twitters} />
+      </Switch>
+    </BrowserRouter>
   </MainTemplate>
 );
 
