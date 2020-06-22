181c82dfec42519ba86b4f8491ff1c25962b9774
s450026 <maksymilian.kierski@gmail.com>
2020-06-22 20:38:58 +0200

    Add redux devtools to app

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from 'redux';
 import notesReducer from 'reducers/notes';
 import filterReducer from 'reducers/filters';
 
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
   combineReducers({
     notes: notesReducer,
     filters: filterReducer,
   }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
+/* eslint-enable */
 export default store;
