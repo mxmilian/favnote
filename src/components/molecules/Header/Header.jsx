772dc5128a93bcad8acba18afaf88c8b6107ea82
s450026 <maksymilian.kierski@gmail.com>
2020-06-28 18:22:23 +0200

    Add styles to select

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -7,6 +7,8 @@ import styled from 'styled-components';
 import Heading from 'components/atoms/Heading/Heading';
 import Input from 'components/atoms/Input/Input';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import arrowDown from 'assets/arrow-down.svg';
+import arrowUp from 'assets/arrow-up.svg';
 import {
   setTextFilter as setTextFilterAction,
   setSortBy as setSortByAction,
@@ -25,10 +27,26 @@ const StyledParagraph = styled(Paragraph)`
 
 const StyledFilters = styled.div`
   display: flex;
+  align-content: center;
+  margin: 2rem 0 3rem 0;
 `;
 
-const StyledInput = styled(Input)`
-  margin: 2rem 0;
+const StyledSelect = styled.select`
+  margin-left: 2rem;
+  background-image: url(${({ value }) => (value === 'asc' ? arrowDown : arrowUp)});
+  font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: ${({ theme }) => theme.regular};
+  background-color: ${({ theme }) => theme.grey100};
+  border: none;
+  border-radius: 5rem;
+  padding: 1rem 2rem 1rem 4rem;
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  background-size: 1.5rem;
+  background-position: 1.5rem 50%;
+  background-repeat: no-repeat;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  color: ${({ theme }) => theme.black};
 `;
 
 const Header = ({ pageContext, notes, text, sortBy, setFilterText, setSortBy }) => (
@@ -40,11 +58,11 @@ const Header = ({ pageContext, notes, text, sortBy, setFilterText, setSortBy }) 
       {notes.length} {pageContext}
     </StyledParagraph>
     <StyledFilters>
-      <StyledInput search value={text} onChange={(e) => setFilterText(e.target.value)} />
-      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+      <Input search value={text} onChange={(e) => setFilterText(e.target.value)} />
+      <StyledSelect value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
-      </select>
+      </StyledSelect>
     </StyledFilters>
   </>
 );
