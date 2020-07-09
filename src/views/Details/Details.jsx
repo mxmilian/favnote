e7e6ce1462a7c9cfb5d10e936903ab46af35883b
s450026 <maksymilian.kierski@gmail.com>
2020-07-09 13:58:16 +0200

    Connect details page to our store

--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
-import { notes } from 'data/dummyData';
+import withContext from 'hoc/withContext';
+import { connect } from 'react-redux';
 
-const Details = () => (
-  <DetailsTemplate
-    title={notes[0].title}
-    createdAt={notes[0].createdAt}
-    content={notes[0].content}
-    articleUrl={notes[0].articleUrl}
-    twitterName="https://i.kym-cdn.com/entries/icons/facebook/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg"
-  />
-);
+class Details extends Component {
+  componentDidMount() {}
 
-export default Details;
+  render() {
+    const { activeItem } = this.props;
+    const [item] = activeItem;
+    return (
+      <DetailsTemplate
+        title={item.title}
+        createdAt={item.createdAt}
+        content={item.content}
+        articleUrl={item.articleUrl}
+        twitterName="https://i.kym-cdn.com/entries/icons/facebook/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg"
+      />
+    );
+  }
+}
+
+Details.propTypes = {
+  activeItem: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+const mapStateToProps = ({ notes }, { pageContext, match }) => ({
+  activeItem: notes[pageContext].filter(({ _id: id }) => id === match.params.id),
+});
+
+export default withContext(connect(mapStateToProps)(Details));
