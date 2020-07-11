7cb8a33e5aae1149d599f1465629c5a797666201
s450026 <maksymilian.kierski@gmail.com>
2020-07-11 22:07:37 +0200

    Remove grey border around the image

--- src/components/atoms/Image/Image.jsx
+++ src/components/atoms/Image/Image.jsx
@@ -5,11 +5,10 @@ const Image = styled.div`
   width: 10.7rem;
   height: 10.7rem;
   border-radius: 100%;
-  border: 1px solid white;
-  background: white url(${({ icon }) => icon}) no-repeat 50% 50%;
+  border: none;
+  background: url(${({ icon }) => icon}) no-repeat 50% 50%;
   background-size: 100% 100%;
   overflow: hidden;
-  content: '';
 `;
 
 export default Image;
