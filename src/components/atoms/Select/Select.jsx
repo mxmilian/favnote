35667a7f598b29dcb2cc0fb504cd74c99334ed61
s450026 <maksymilian.kierski@gmail.com>
2020-07-23 16:13:55 +0200

    Style the dropdown

--- src/components/atoms/Select/Select.jsx
+++ src/components/atoms/Select/Select.jsx
@@ -6,21 +6,23 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import cx from 'classnames';
 
-const StyledSelectButton = styled.button`
+const StyledWrapper = styled.div`
   margin-left: 2rem;
+`;
+
+const StyledSelectButton = styled.button`
   background-image: url(${({ value }) => (value === 'asc' ? arrowDown : arrowUp)});
   font-size: ${({ theme }) => theme.fontSize.s};
   font-weight: ${({ theme }) => theme.regular};
   background-color: ${({ theme }) => theme.grey100};
   border: none;
   border-radius: 5rem;
-  padding: 1rem 2rem 1rem 4rem;
+  padding: 1rem 2rem 1rem 3rem;
   font-size: ${({ theme }) => theme.fontSize.xs};
   background-size: 1.5rem;
   background-position: 1.5rem 50%;
   background-repeat: no-repeat;
   text-transform: uppercase;
-  letter-spacing: 1px;
   color: ${({ theme }) => theme.black};
   min-width: 20rem;
 
@@ -37,40 +39,42 @@ const Select = ({ items, setSortBy }) => (
     }}
     itemToString={(item) => (item ? item.value : '')}
   >
-    {({ getItemProps, isOpen, toggleMenu, selectedItem, getMenuProps }) => (
-      <div
-        className={cx('dropdown', {
-          'is-active': isOpen,
-        })}
-      >
-        <StyledSelectButton
-          id="my-select"
-          type="button"
-          onClick={toggleMenu}
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded={isOpen}
-          value="desc"
+    {({ getItemProps, isOpen, toggleMenu, selectedItem, getMenuProps, getRootProps }) => (
+      <StyledWrapper {...getRootProps()}>
+        <div
+          className={cx('dropdown', {
+            'is-active': isOpen,
+          })}
         >
-          {selectedItem ? selectedItem.name : 'Select an item'}
-        </StyledSelectButton>
-        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-          <div className="dropdown-content" {...getMenuProps()}>
-            {items.map((item) => (
-              <button
-                type="button"
-                key={item.value}
-                className={cx('dropdown-item', 'button', 'is-white', {
-                  'is-active': item.value === selectedItem,
-                })}
-                {...getItemProps({ item })}
-              >
-                {item.name}
-              </button>
-            ))}
+          <StyledSelectButton
+            id="my-select"
+            type="button"
+            onClick={toggleMenu}
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded={isOpen}
+            value="desc"
+          >
+            {selectedItem ? selectedItem.name : 'Select an order'}
+          </StyledSelectButton>
+          <div className="dropdown-menu" id="dropdown-menu" role="menu">
+            <div className="dropdown-content" {...getMenuProps()}>
+              {items.map((item) => (
+                <button
+                  type="button"
+                  key={item.value}
+                  className={cx('dropdown-item', 'button', 'is-white', 'is-size-5', {
+                    'is-active': item.value === selectedItem ? selectedItem.value : '',
+                  })}
+                  {...getItemProps({ item })}
+                >
+                  {item.name}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </StyledWrapper>
     )}
   </Downshift>
 );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root/Root';
-import 'bulma';
+import 'theme/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
