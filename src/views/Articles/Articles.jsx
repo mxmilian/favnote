ee07867b7c80301a42eb514a1aa24be661d45e17
s450026 <maksymilian.kierski@gmail.com>
2020-07-28 20:04:41 +0200

    Change class components Articles to function components with hooks

--- src/views/Articles/Articles.jsx
+++ src/views/Articles/Articles.jsx
@@ -1,42 +1,41 @@
 import { fetchNotes as fetchNotesAction } from 'actions/notes';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import getVisibleNotes from 'selector';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
 import withLoader from 'hoc/withLoader';
+import { useFetchData } from 'hooks/useFetchData';
 
-class Articles extends Component {
-  componentDidMount() {
-    const {
-      props: { fetchNotes, toggleLoading },
-    } = this;
-    const { articles } = this.props;
-    if (articles.length === 0) {
-      toggleLoading();
-      fetchNotes('articles').then(() => toggleLoading());
-    }
-  }
+const Articles = ({ articles, loading, fetchNotes, toggleLoading }) => {
+  // componentDidMount() {
+  //   const {
+  //     props: { fetchNotes, toggleLoading },
+  //   } = this;
+  //   const { articles } = this.props;
+  //   if (articles.length === 0) {
+  //     toggleLoading();
+  //     fetchNotes('articles').then(() => toggleLoading());
+  //   }
+  // }
+  useFetchData(fetchNotes, articles, 'articles', toggleLoading);
 
-  render() {
-    const { articles, loading } = this.props;
-    return (
-      <GridTemplate loading={loading}>
-        {articles.map(({ _id: id, title, createdAt, content, articleUrl }) => (
-          <Card
-            id={id}
-            key={id}
-            title={title}
-            createdAt={createdAt}
-            content={content}
-            articleUrl={articleUrl}
-          />
-        ))}
-      </GridTemplate>
-    );
-  }
-}
+  return (
+    <GridTemplate loading={loading}>
+      {articles.map(({ _id: id, title, createdAt, content, articleUrl }) => (
+        <Card
+          id={id}
+          key={id}
+          title={title}
+          createdAt={createdAt}
+          content={content}
+          articleUrl={articleUrl}
+        />
+      ))}
+    </GridTemplate>
+  );
+};
 
 Articles.propTypes = {
   articles: PropTypes.arrayOf(
