2f4b0106ff1d6df3f3b3b07f66b79004b5effc2b
s450026 <maksymilian.kierski@gmail.com>
2020-08-14 15:57:31 +0200

    Change sign comonenpt to funtional

--- src/views/Sign/Sign.jsx
+++ src/views/Sign/Sign.jsx
@@ -1,7 +1,7 @@
 import Input from 'components/atoms/Input/Input';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Loader from 'react-loader-spinner';
 import { routes } from 'routes';
 import styled from 'styled-components';
@@ -71,204 +71,195 @@ const StyledErrorWrapper = styled.div`
   margin-top: 1rem;
 `;
 
-class Sign extends Component {
-  state = {
-    signUp: false,
-    isEmail: false,
-    visible: true,
+const Sign = ({ authenticate, register, userID, loading, toggleLoading, failure }) => {
+  const [signUp, setSignUp] = useState(false);
+  const [isEmail, setIsEmail] = useState(false);
+  const [visible, setVisible] = useState(true);
+
+  const toggleSign = () => {
+    setSignUp((prevState) => !prevState);
   };
 
-  toggleSign = () => {
-    this.setState((prevState) => ({ signUp: !prevState.signUp }));
+  const toggleEmail = () => {
+    setIsEmail((prevState) => !prevState);
   };
 
-  toggleEmail = () => {
-    this.setState((prevState) => ({ isEmail: !prevState.isEmail }));
+  const toggleVisible = () => {
+    setVisible((prevState) => !prevState);
   };
-
-  toggleVisible = () => {
-    this.setState((prevState) => ({ visible: !prevState.visible }));
-  };
-
-  render() {
-    const { signUp, isEmail, visible } = this.state;
-    const { toggleSign, toggleEmail, toggleVisible } = this;
-    const { authenticate, register, userID, loading, toggleLoading, failure } = this.props;
-    if (userID) return <Redirect push to={routes.notes} />;
-    return (
-      <AuthTemplate>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-          }}
-          validationSchema={Yup.object({
-            name:
-              isEmail || !signUp
-                ? ''
-                : Yup.string()
-                    .max(30, 'Must be 30 characters or less')
-                    .min(8, 'Must be 8 characters or more')
-                    .required('Required'),
-            email:
-              signUp || (isEmail && signUp)
-                ? Yup.string()
-                    .email('Must be email')
-                    .max(40, 'Must be 40 characters or less')
-                    .required('Required')
-                : '',
-            password: signUp
-              ? Yup.string()
+  if (userID) return <Redirect push to={routes.notes} />;
+  return (
+    <AuthTemplate>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
+        }}
+        validationSchema={Yup.object({
+          name:
+            isEmail || !signUp
+              ? ''
+              : Yup.string()
                   .max(30, 'Must be 30 characters or less')
                   .min(8, 'Must be 8 characters or more')
-                  .required('Required')
-              : '',
-            confirmPassword: signUp
+                  .required('Required'),
+          email:
+            signUp || (isEmail && signUp)
               ? Yup.string()
-                  .max(30, 'Must be 30 characters or less')
-                  .min(8, 'Must be 8 characters or more')
+                  .email('Must be email')
+                  .max(40, 'Must be 40 characters or less')
                   .required('Required')
               : '',
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(false);
-            if (!visible) toggleVisible();
-            toggleLoading();
-            try {
-              if (!signUp) {
-                if (isEmail) {
-                  authenticate(null, values.email, values.password)
-                    .then(() => toggleLoading())
-                    .catch(() => toggleLoading());
-                } else if (!isEmail) {
-                  authenticate(values.name, null, values.password)
-                    .then(() => toggleLoading())
-                    .catch(() => toggleLoading());
-                }
-              } else {
-                register(values.name, values.email, values.password, values.confirmPassword)
+          password: signUp
+            ? Yup.string()
+                .max(30, 'Must be 30 characters or less')
+                .min(8, 'Must be 8 characters or more')
+                .required('Required')
+            : '',
+          confirmPassword: signUp
+            ? Yup.string()
+                .max(30, 'Must be 30 characters or less')
+                .min(8, 'Must be 8 characters or more')
+                .required('Required')
+            : '',
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(false);
+          if (!visible) toggleVisible();
+          toggleLoading();
+          try {
+            if (!signUp) {
+              if (isEmail) {
+                authenticate(null, values.email, values.password)
+                  .then(() => toggleLoading())
+                  .catch(() => toggleLoading());
+              } else if (!isEmail) {
+                authenticate(values.name, null, values.password)
                   .then(() => toggleLoading())
                   .catch(() => toggleLoading());
               }
-            } catch (e) {
-              console.log('Not logged');
-              console.log(e);
+            } else {
+              register(values.name, values.email, values.password, values.confirmPassword)
+                .then(() => toggleLoading())
+                .catch(() => toggleLoading());
             }
-          }}
-        >
-          {(formik) => (
-            <>
-              <StyledHeading>{userID}</StyledHeading>
-              <StyledHeading>{signUp ? 'Sign up' : 'Sign in'}</StyledHeading>
-              {failure && visible ? (
-                <StyledErrorWrapper>
-                  <Error failure={failure} visible={visible} setVisible={toggleVisible} />
-                </StyledErrorWrapper>
-              ) : null}
-              <StyledForm>
-                {signUp ? null : (
-                  <>
-                    <StyledInput
-                      id={isEmail ? 'email' : 'name'}
-                      name={isEmail ? 'email' : 'name'}
-                      type="text"
-                      onChange={(e) => {
-                        if (e.target.value && e.target.value.includes('@') && !isEmail) {
-                          toggleEmail();
-                          // eslint-disable-next-line no-param-reassign
-                          formik.setFieldValue('email', e.target.value);
-                        }
-                        if (formik.values.email && !e.target.value.includes('@') && isEmail) {
-                          toggleEmail();
-                          // eslint-disable-next-line no-param-reassign
-                          formik.setFieldValue('name', e.target.value);
-                        }
-                        formik.handleChange(e);
-                      }}
-                      onBlur={formik.handleBlur}
-                      value={isEmail ? formik.values.email : formik.values.name}
-                      placeholder="email or name"
-                    />
-                    {/* eslint-disable-next-line no-nested-ternary */}
-                    {isEmail ? (
-                      formik.touched.email && formik.errors.email ? (
-                        <StyledError>
-                          <StyledParagraph>{formik.errors.email}!</StyledParagraph>
-                        </StyledError>
-                      ) : null
-                    ) : formik.touched.name && formik.errors.name ? (
-                      <StyledError>
-                        <StyledParagraph>{formik.errors.name}!</StyledParagraph>
-                      </StyledError>
-                    ) : null}
-                  </>
-                )}
-                {signUp ? (
-                  <>
-                    <StyledInput placeholder="name" {...formik.getFieldProps('name')} />
-                    {formik.touched.name && formik.errors.name ? (
-                      <StyledError>
-                        <StyledParagraph>{formik.errors.name}!</StyledParagraph>
-                      </StyledError>
-                    ) : null}
-                  </>
-                ) : null}
-
-                {signUp ? (
-                  <>
-                    <StyledInput placeholder="email" {...formik.getFieldProps('email')} />
-                    {formik.touched.email && formik.errors.email ? (
+          } catch (e) {
+            console.log('Not logged');
+          }
+        }}
+      >
+        {(formik) => (
+          <>
+            <StyledHeading>{userID}</StyledHeading>
+            <StyledHeading>{signUp ? 'Sign up' : 'Sign in'}</StyledHeading>
+            {failure && visible ? (
+              <StyledErrorWrapper>
+                <Error failure={failure} visible={visible} setVisible={toggleVisible} />
+              </StyledErrorWrapper>
+            ) : null}
+            <StyledForm>
+              {signUp ? null : (
+                <>
+                  <StyledInput
+                    id={isEmail ? 'email' : 'name'}
+                    name={isEmail ? 'email' : 'name'}
+                    type="text"
+                    onChange={(e) => {
+                      if (e.target.value && e.target.value.includes('@') && !isEmail) {
+                        toggleEmail();
+                        // eslint-disable-next-line no-param-reassign
+                        formik.setFieldValue('email', e.target.value);
+                      }
+                      if (formik.values.email && !e.target.value.includes('@') && isEmail) {
+                        toggleEmail();
+                        // eslint-disable-next-line no-param-reassign
+                        formik.setFieldValue('name', e.target.value);
+                      }
+                      formik.handleChange(e);
+                    }}
+                    onBlur={formik.handleBlur}
+                    value={isEmail ? formik.values.email : formik.values.name}
+                    placeholder="email or name"
+                  />
+                  {/* eslint-disable-next-line no-nested-ternary */}
+                  {isEmail ? (
+                    formik.touched.email && formik.errors.email ? (
                       <StyledError>
                         <StyledParagraph>{formik.errors.email}!</StyledParagraph>
                       </StyledError>
-                    ) : null}
-                  </>
-                ) : null}
-                <StyledInput
-                  type="password"
-                  placeholder="password"
-                  {...formik.getFieldProps('password')}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <StyledError>
-                    <StyledParagraph>{formik.errors.password}!</StyledParagraph>
-                  </StyledError>
-                ) : null}
-                {signUp ? (
-                  <>
-                    <StyledInput
-                      type="password"
-                      placeholder="Confirm password"
-                      {...formik.getFieldProps('confirmPassword')}
-                    />
-                    {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-                      <StyledError>
-                        <StyledParagraph>{formik.errors.confirmPassword}!</StyledParagraph>
-                      </StyledError>
-                    ) : null}
-                  </>
-                ) : null}
-                {loading ? (
-                  <StyledLoader type="Oval" color={themeLoader.notes} height={50} width={50} />
-                ) : (
-                  <StyledButton activecolor="notes" type="submit">
-                    {signUp ? 'Sign up' : 'Sign in'}
-                  </StyledButton>
-                )}
-              </StyledForm>
+                    ) : null
+                  ) : formik.touched.name && formik.errors.name ? (
+                    <StyledError>
+                      <StyledParagraph>{formik.errors.name}!</StyledParagraph>
+                    </StyledError>
+                  ) : null}
+                </>
+              )}
+              {signUp ? (
+                <>
+                  <StyledInput placeholder="name" {...formik.getFieldProps('name')} />
+                  {formik.touched.name && formik.errors.name ? (
+                    <StyledError>
+                      <StyledParagraph>{formik.errors.name}!</StyledParagraph>
+                    </StyledError>
+                  ) : null}
+                </>
+              ) : null}
 
-              <StyledLink onClick={toggleSign}>
-                {signUp ? 'I want to sign in' : 'I want to sign up'}
-              </StyledLink>
-            </>
-          )}
-        </Formik>
-      </AuthTemplate>
-    );
-  }
-}
+              {signUp ? (
+                <>
+                  <StyledInput placeholder="email" {...formik.getFieldProps('email')} />
+                  {formik.touched.email && formik.errors.email ? (
+                    <StyledError>
+                      <StyledParagraph>{formik.errors.email}!</StyledParagraph>
+                    </StyledError>
+                  ) : null}
+                </>
+              ) : null}
+              <StyledInput
+                type="password"
+                placeholder="password"
+                {...formik.getFieldProps('password')}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <StyledError>
+                  <StyledParagraph>{formik.errors.password}!</StyledParagraph>
+                </StyledError>
+              ) : null}
+              {signUp ? (
+                <>
+                  <StyledInput
+                    type="password"
+                    placeholder="Confirm password"
+                    {...formik.getFieldProps('confirmPassword')}
+                  />
+                  {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                    <StyledError>
+                      <StyledParagraph>{formik.errors.confirmPassword}!</StyledParagraph>
+                    </StyledError>
+                  ) : null}
+                </>
+              ) : null}
+              {loading ? (
+                <StyledLoader type="Oval" color={themeLoader.notes} height={50} width={50} />
+              ) : (
+                <StyledButton activecolor="notes" type="submit">
+                  {signUp ? 'Sign up' : 'Sign in'}
+                </StyledButton>
+              )}
+            </StyledForm>
+
+            <StyledLink onClick={toggleSign}>
+              {signUp ? 'I want to sign in' : 'I want to sign up'}
+            </StyledLink>
+          </>
+        )}
+      </Formik>
+    </AuthTemplate>
+  );
+};
 Sign.propTypes = {
   authenticate: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
