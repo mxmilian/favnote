10ec88d60d30c006ff115b981bcd444629b3a129
s450026 <maksymilian.kierski@gmail.com>
2020-08-17 17:55:24 +0200

    Improve useCurrentPage hook

--- src/hooks/useCurrentPage.js
+++ src/hooks/useCurrentPage.js
@@ -4,11 +4,14 @@ import { NOTES, TWITTERS, ARTICLES, USERS } from 'utils/constants';
 export const useCurrentPage = (location) => {
   const [pageType, setPageType] = useState(NOTES);
   const pageTypes = [NOTES, TWITTERS, ARTICLES, USERS];
+  const page = location.pathname.split('/')[1];
 
   useEffect(() => {
-    const [currentPage] = pageTypes.filter((el) => location.pathname.includes(el));
-    if (pageType !== currentPage && currentPage !== undefined) setPageType(currentPage);
-  }, [location.pathname.split('/')[1]]);
+    const [currentPage] = pageTypes.filter((el) => page.includes(el));
+    if (pageType !== currentPage && currentPage !== undefined) {
+      setPageType(currentPage);
+    }
+  }, [page, pageType, pageTypes]);
 
   return pageType;
 };
