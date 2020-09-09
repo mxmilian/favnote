fd72f7f94bdc6c0652828783acfa20aafb003439
s450026 <maksymilian.kierski@gmail.com>
2020-09-09 21:49:39 +0200

    Style ButtonPhoto

--- src/components/atoms/ButtonPhoto/ButtonPhoto.jsx
+++ src/components/atoms/ButtonPhoto/ButtonPhoto.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import cameraImg from 'assets/camera.svg';
 
-const ButtonPhoto = styled.button`
+const StyledButtonPhoto = styled.button`
+  position: relative;
   display: block;
   width: 25.7rem;
   height: 25.7rem;
@@ -9,15 +13,41 @@ const ButtonPhoto = styled.button`
   background: url(${({ icon }) => icon}) no-repeat 50% 50%;
   background-size: 100% 100%;
   cursor: pointer;
-  display: block;
 
   @media (max-width: 375px) {
     background-size: 40% 40%;
   }
+`;
 
-  &.active {
-    background-color: white;
+const StyledButtonHover = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 25.7rem;
+  height: 25.7rem;
+  background: url(${() => cameraImg}) no-repeat 50% 50% hsla(0, 0%, 100%, 50%);
+  background-size: 50% 50%;
+  border-radius: 100rem;
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 0.4s;
+
+  &:hover {
+    opacity: 1;
+    transition: opacity 0.4s;
   }
 `;
+
+const ButtonPhoto = ({ icon }) => (
+  <>
+    <StyledButtonPhoto icon={icon} onClick={() => console.log('Xd')}>
+      <StyledButtonHover />
+    </StyledButtonPhoto>
+  </>
+);
+
+ButtonPhoto.propTypes = {
+  icon: PropTypes.string.isRequired,
+};
 
 export default ButtonPhoto;
